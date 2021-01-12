f8bbd8a9769498a83a98ed8b9c35c64f0850054e
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-12 14:55:48 +0100

    Demo DataAdapter

---
+++ DemoDataAdapter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DemoDataAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string sConnectionString = "Initial Catalog= BierenDb;Data Source=localhost;Integrated Security=SSPI;";
+            using (SqlConnection conn = new SqlConnection(sConnectionString))
+            {
+                SqlDataAdapter sqlAdp = new SqlDataAdapter("Select * from Bieren", conn);
+                DataSet sqlDS = new DataSet(" bierentabel");
+                sqlAdp.Fill(sqlDS, "bierentabel");
+                DataTableReader dtReader = sqlDS.Tables["bierentabel"].CreateDataReader();
+
+                while (dtReader.Read())
+                {
+                    for (int i = 0; i < dtReader.FieldCount; i++) //FieldCount geeft het aantal kolommen terug
+                    {  // ………   
+                        Console.Write(dtReader[i] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
