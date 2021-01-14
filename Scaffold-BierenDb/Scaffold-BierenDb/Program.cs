68db0b53a5b0e0d25c2f495cd770c5886bc96742
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-14 09:47:22 +0100

    Toevoegen van nieuw bier via EF Core

--- Scaffold-BierenDb/Scaffold-BierenDb/Program.cs
+++ Scaffold-BierenDb/Scaffold-BierenDb/Program.cs
@@ -32,7 +32,43 @@ namespace Scaffold_BierenDb
             }
 
             //Oefening: Geef alle soorten en voor elke soort alle bieren
-            Console.ReadKey();
+            /*
+             * Oefening BierenDb. Voeg een nieuw bier toe met naam ”TESTBIER” met soort “Alcoholvrij” 
+             * (zoek eerst het SoortNr op in Soorten) aan de Brouwer “Zwingel” (zoek eerst deze Brouwer op via BrNaam). 
+             * Bewaar de gegevens in de BierenDb
+            */
+
+            //zoek eerst het SoortNr op in Soorten met naam “Alcoholvrij” 
+            using (BierenDbContext db = new BierenDbContext())
+            {
+                Soorten soort = db.Soorten.Where(s => s.Soort.ToLower() == "alcoholvrij").FirstOrDefault();
+                if (soort == null)
+                {
+                    Console.WriteLine("Soort 'alcoholvrij' niet gevonden");
+                }
+                else
+                {
+                    //Zoek BrouwerNr voor Brouwer “Zwingel” (zoek eerst deze Brouwer op via BrNaam)
+                    Brouwers brouwer = db.Brouwers.Where(b => b.BrNaam.ToLower() == "zwingel").Single();
+                    //Nieuw Bier toevoegen met naam "TESTBIER" van brouwer "Zwingel" en soort "alcoholvrij"
+                    int newBierNr = db.Bieren.Max(b => b.BierNr) + 1;
+                    Bieren bier = new Bieren()
+                    {
+                        BierNr = newBierNr, //99999,
+                        Naam = "TESTBIER",
+                        Alcohol = 0.0,
+                        BrouwerNr = brouwer.BrouwerNr,
+                        SoortNr = soort.SoortNr
+                    };
+                    //Toevoegen aan de collectie Bierens
+                    db.Bieren.Add(bier);
+                    //Nieuw bier bewaren in database
+                    db.SaveChanges();
+                }
+
+                //}
+
+                Console.ReadKey();
         }
-    }
+}
 }
